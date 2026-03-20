6508c010bcc246e81ad698b35b447ad1cdfa5ca1
virginiacsn <virginiacasasnovas@gmail.com>
2026-03-20 15:13:44 -0400

    Added deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+// import { defineConfig } from "astro/config";
+// import mdx from "@astrojs/mdx";
+// import sitemap from "@astrojs/sitemap";
+// import tailwind from "@astrojs/tailwind";
+
+// export default defineConfig({
+//   site: "https://astro-nano-demo.vercel.app",
+//   integrations: [mdx(), sitemap(), tailwind()],
+// });
+
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
-  integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://virginiacsn.github.io',
 });
