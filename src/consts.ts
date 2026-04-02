29692fcbd98f439e8eaa6c8f619c61cdc022a9a5
virginiacsn <virginiacasasnovas@gmail.com>
2026-04-02 11:45:22 -0400

    Updated consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Virginia Casasnovas",
-  EMAIL: "",
+  EMAIL: "virginiacasasnovas@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -44,6 +44,6 @@ export const SOCIALS: Socials = [
   },
   {
     NAME: "google scholar",
-    HREF: "https://scholar.google.com/citations?user=YOUR_ID",
+    HREF: "https://scholar.google.com/citations?user=1umY3nIAAAAJ&hl=en",
   }
 ];
